e6ccdb2bde4eca4a0fa9b84b80b6b026923b6d18
dudosyka <a.hatson22@gmail.com>
2023-02-20 21:05:31 +0300

    Vuetify registration

--- chat/src/main.ts
+++ chat/src/main.ts
@@ -3,5 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).use(store).use(router).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).use(store).use(router).mount('#app')
