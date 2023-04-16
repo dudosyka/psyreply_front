58b2d9eaf2a957bf224c1dacbacd4a24566071a9
dudosyka <a.hatson22@gmail.com>
2023-04-16 15:14:46 +0300

    remove admin

--- admin/src/api/admin/QuestionType.js
+++
@@ -1,7 +0,0 @@
-import Request from '@/api';
-
-export default class QuestionType extends Request {
-  constructor(id = 0) {
-    super('question-type', id);
-  }
-}
--- admin/src/api/admin/distribution/DIstributionMessageType.js
+++
@@ -1,6 +0,0 @@
-export default {
-  TEXT: 1,
-  MEDIA: 2,
-  LINK: 3,
-  TEST: 4
-}
