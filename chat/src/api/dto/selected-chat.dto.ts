20148a6fc262fc043c900959530f105a5bbac182
dudosyka <a.hatson22@gmail.com>
2023-04-21 01:53:37 +0300

    Import fix

--- chat/src/api/dto/selected-chat.dto.ts
+++ chat/src/api/dto/selected-chat.dto.ts
@@ -1,9 +1,9 @@
 import {ChatInfoDto} from "@/api/dto/chat-info.dto";
 import {UserModelDto} from "@/api/dto/user-model.dto";
-import {ChatMessageModelDto} from "@/api/dto/chat-message-model.dto";
+import {MessageModelDto} from "@/api/dto/message-model.dto";
 
 export type SelectedChatDto = {
   user: UserModelDto,
-  messages: ChatMessageModelDto[],
+  messages: MessageModelDto[],
   info: ChatInfoDto
 }
