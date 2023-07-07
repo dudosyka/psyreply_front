a1d8c04797388888da18b1b6f925d902986d3688
scobca <scobca18@yandex.ru>
2023-07-07 16:25:58 +0300

    Integration 'Popup' notification

---
+++ client/src/components/UI/YPopupWarn.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="popup" v-if="popupWarn.show" @click.stop="close">
+        <div class="ypopup__error">
+            <div class="popup__exit"><img src="@/assets/img/cross.png" width="12rem" alt=""></div>
+            <div class="popup__icon"><img src="@/assets/img/error.svg" alt=""></div>
+            <h1 class="popup__title">Внимание!</h1>
+            <div class="error__code">{{ popupWarn.message }}</div>
+            <div class="controls">
+                <y-button class="accept_control" @click="accept"><i class="fa-solid fa-trash-can"></i> Подтвердить</y-button>
+                <y-button class="rollback_control" @click="close">Отмена</y-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import YButton from "@/components/UI/YButton";
+
+export default {
+    name: "YPopupWarn",
+    components: {YButton},
+    methods: {
+        close() {
+            this.$store.commit('closeWarnPopup')
+        },
+        accept() {
+            this.$store.commit('acceptWarnPopup');
+            this.close();
+        }
+    },
+    computed: {
+        popupWarn() {
+            return this.$store.getters.popupWarn
+        }
+    }
+}
+</script>
+
+<style scoped>
+.accept_control {
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    border-radius: 0.4rem;
+    width: 100%;
+    max-width: 15vw;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    background-color: #ff0062;
+    color: white;
+}
+.accept_control:hover {
+    border: 1px solid rgba(255, 255, 255, 1);
+    border-radius: 0.4rem;
+    width: 100%;
+    max-width: 15vw;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    background-color: #ff0062;
+    color: white;
+}
+.rollback_control {
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    max-width: 15vw;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+}
+.rollback_control:hover {
+    border: 1px solid rgba(255, 255, 255, 1);
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    max-width: 15vw;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+}
+.popup {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(5, 5, 5, 0.67);
+    backdrop-filter: blur(14.5px);
+    z-index: 9999;
+}
+
+.ypopup__error {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    z-index: 9999999;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(140.62deg, rgba(255, 255, 255, 0.25) 2.81%, rgba(255, 255, 255, 0.1) 100.82%);
+    box-shadow: 0px 4px 52px rgba(143, 0, 255, 0.11);
+    border-radius: 16px;
+    padding-top: 1rem;
+    padding-bottom: 4rem;
+    width: 30rem;
+    border-top: rgba(255, 255, 255, 0.5);
+    border-bottom: rgba(255, 255, 255, 0.5);
+    border-right: rgba(255, 255, 255, 0);
+    border-left: rgba(255, 255, 255, 0);
+
+}
+
+.popup__title {
+
+    margin-left: 1rem;
+
+}
+
+.popup__icon {
+    margin-top: 2rem;
+    width: 10rem;
+
+}
+
+.popup__fix {
+    width: 20rem;
+}
+
+.popup__exit {
+    align-self: end;
+}
+
+.popup__exit:hover {
+    cursor: pointer;
+}
+
+.error__code__title {
+    margin-top: 0.5rem;
+    opacity: 0.7;
+    font-size: 0.9rem;
+}
+
+.error__code {
+    font-size: 1.2rem;
+    margin-top: 0.5rem;
+    color: rgba(251, 128, 255, 0.58);
+}
+
+.controls {
+    width: 80%;
+    margin-top: 1rem;
+    display: flex;
+    justify-content: space-around;
+}
+</style>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,264 +1,316 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 import Client from '@/api/Client';
 import axios from "axios";
 
 export default createStore({
-  state: {
-    blockOnPass: null,
-    passedBlock: null,
-    allDataIsReady: false,
-    allResultsIsReady: false,
-    answersCount: null,
-    answersPassed: null,
-    // not use because use localStorage - it's crunch (((
-    // testToken: null,
-    // resultsToken: null,
-    view: null,
-    userId: null,
-    results: null,
-    tokens: [
-        '5783059057:AAFKjXFRicrIanLozc5RTue_Hl7y_imwY1A',
-        '5208325504:AAEKGs0MqFVQ-NHJnuzXqKol9H-fkOS9-YM',
-        '6069143205:AAFbUz4h_fdrdgGxujkQsrZWKYWOJdF0Vsk',
-        '5927214631:AAGhrTprF62v7o1MTQjqHicTye6-VPkLneE'
-    ]
-  },
-  getters: {
-    // not use because use localStorage - it's crunch (((
-    // testToken(state) {
-    //   return state.testToken
-    // },
-    // resultsToken(state) {
-    //   return state.resultsToken
-    // },
-    blockOnPass(state) {
-      return state.blockOnPass
+    state: {
+        blockOnPass: null,
+        passedBlock: null,
+        allDataIsReady: false,
+        allResultsIsReady: false,
+        answersCount: null,
+        answersPassed: null,
+        // not use because use localStorage - it's crunch (((
+        // testToken: null,
+        // resultsToken: null,
+        view: null,
+        userId: null,
+        results: null,
+        tokens: [
+            '5783059057:AAFKjXFRicrIanLozc5RTue_Hl7y_imwY1A',
+            '5208325504:AAEKGs0MqFVQ-NHJnuzXqKol9H-fkOS9-YM',
+            '6069143205:AAFbUz4h_fdrdgGxujkQsrZWKYWOJdF0Vsk',
+            '5927214631:AAGhrTprF62v7o1MTQjqHicTye6-VPkLneE'
+        ],
+        app: {
+            popupError: {
+                show: false,
+                message: null
+            },
+            popupWarn: {
+                show: false,
+                message: null,
+                acceptCallback: null,
+            },
+            popup: {
+                show: false,
+                message: null
+            },
+        },
     },
-    passedBlock(state) {
-      return state.passedBlock
+    getters: {
+          // not use because use localStorage - it's crunch (((
+        // testToken(state) {
+        //   return state.testToken
+        // },
+        // resultsToken(state) {
+        //   return state.resultsToken
+        // },
+        blockOnPass(state) {
+            return state.blockOnPass
+        },
+        passedBlock(state) {
+            return state.passedBlock
+        },
+        results(state) {
+            return state.results
+        },
+        isAllDataReady(state) {
+            return state.allDataIsReady
+        },
+        isAllResultsReady(state) {
+            return state.allResultsIsReady
+        },
+        questionData: (state) => (coordinates) => {
+            const test = state.blockOnPass.tests[coordinates.test_id]
+            return test.questions[coordinates.question_id]
+        },
+        questionByGroupData: (state) => (coordinates) => {
+            const test = state.blockOnPass.tests[coordinates.test_id]
+            const questionsGroup = test.questions[coordinates.question_arr_id]
+            return questionsGroup[coordinates.question_id]
+        },
+        passedBlockAnswer: (state) => (coordinates) => {
+            const test = state.passedBlock.tests[coordinates.test_id]
+            return test.answers[coordinates.answer_id].answer
+        },
+        relationAnswersAndPassedAnswers(state) {
+            return Math.floor(state.answersPassed / state.answersCount * 100)
+        },
+        view(state) {
+            return state.view
+        },
+        popupError(state) {
+            return state.app.popupError
+        },
+        popupWarn(state) {
+            return state.app.popupWarn
+        },
+        popup(state) {
+            return state.app.popup
+        },
     },
-    results(state) {
-      return state.results
-    },
-    isAllDataReady(state) {
-      return state.allDataIsReady
-    },
-    isAllResultsReady(state) {
-      return state.allResultsIsReady
-    },
-    questionData: (state) => (coordinates) => {
-      const test = state.blockOnPass.tests[coordinates.test_id]
-      return test.questions[coordinates.question_id]
-    },
-    questionByGroupData: (state) => (coordinates) => {
-      const test = state.blockOnPass.tests[coordinates.test_id]
-      const questionsGroup = test.questions[coordinates.question_arr_id]
-      return questionsGroup[coordinates.question_id]
-    },
-    passedBlockAnswer: (state) => (coordinates) => {
-      const test = state.passedBlock.tests[coordinates.test_id]
-      return test.answers[coordinates.answer_id].answer
-    },
-    relationAnswersAndPassedAnswers(state) {
-      return Math.floor(state.answersPassed / state.answersCount * 100  )
-    },
-    view(state) {
-      return state.view
-    },
-  },
-  mutations: {
-    updateBlockOnPass(state, block) {
-      state.blockOnPass = block
-    },
-    updatePassedBlock(state, block) {
-      state.passedBlock = block
-    },
-    updateResults(state, results) {
-      state.results = results
-    },
-    allDataIsReady(state) {
-      state.allDataIsReady = true
-    },
-    allResultsIsReady(state) {
-      state.allResultsIsReady = true
-    },
-    selectAnswer(state, data) {
-      const test = state.passedBlock.tests[data.test_id]
-      test.answers[data.question_id].answer = data.answer
-    },
-    setTimeOnPass(state, data) {
-      state.passedBlock.time_on_pass = data
-    },
-    answersPassedIncrement(state) {
-      state.answersPassed++
-    },
-    answersPassedDecrement(state) {
-      state.answersPassed--
-    },
-    setAnswersCount(state, data) {
-      state.answersCount = data
-    },
-    // Not use, because tokens in localStorage - it's crunch (((
-    // setToken(state, data) {
-    //   state[`${data.type}Token`] = data.token
-    //   state.view = data.type
-    // },
-    updateUserId(state, data) {
-      state.userId = data
-    }
-  },
-  actions: {
-    async getBlock({ commit }) {
-      const client = new Client()
+    mutations: {
+        updateBlockOnPass(state, block) {
+            state.blockOnPass = block
+        },
+        updatePassedBlock(state, block) {
+            state.passedBlock = block
+        },
+        updateResults(state, results) {
+            state.results = results
+        },
+        allDataIsReady(state) {
+            state.allDataIsReady = true
+        },
+        allResultsIsReady(state) {
+            state.allResultsIsReady = true
+        },
+        selectAnswer(state, data) {
+            const test = state.passedBlock.tests[data.test_id]
+            test.answers[data.question_id].answer = data.answer
+        },
+        setTimeOnPass(state, data) {
+            state.passedBlock.time_on_pass = data
+        },
+        answersPassedIncrement(state) {
+            state.answersPassed++
+        },
+        answersPassedDecrement(state) {
+            state.answersPassed--
+        },
+        setAnswersCount(state, data) {
+            state.answersCount = data
+        },
+        // Not use, because tokens in localStorage - it's crunch (((
+        // setToken(state, data) {
+        //   state[`${data.type}Token`] = data.token
+        //   state.view = data.type
+        // },
+        updateUserId(state, data) {
+            state.userId = data
+        },
+        openErrorPopup(state, message) {
+            state.app.popupError.show = true
+            state.app.popupError.message = message
+        },
+        closeErrorPopup(state) {
+            state.app.popupError.show = false
+            state.app.popupError.message = ''
+        },
+        openWarnPopup(state, {message, acceptCallback}) {
+            state.app.popupWarn.show = true;
+            state.app.popupWarn.message = message;
+            state.app.popupWarn.acceptCallback = acceptCallback;
+        },
+        acceptWarnPopup(state) {
+            state.app.popupWarn.acceptCallback();
+        },
+        closeWarnPopup(state) {
+            state.app.popupWarn.show = false;
+            state.app.popupWarn.message = "";
+        },
+        openPopup(state, message) {
+            state.app.popup.show = true
+            state.app.popup.message = message
+            setTimeout(() => {
+                state.app.popup.show = false
+                state.app.popup.message = ''
+            }, 3000)
+        },
 
-      const token = localStorage.getItem('testToken')
+    },
+    actions: {
+        async getBlock({commit}) {
+            const client = new Client()
 
-      client.getBlock(token)
-        .then(r => {
-          const passedBlock = {
-            time_on_pass: 0,
-            tests: []
-          }
-          r.tests.forEach(test => {
-            if (test.type_id == 6 || test.type_id == 7)
-              return;
-            passedBlock.tests.push({
-              test_id: test.id,
-              answers: []
+            const token = localStorage.getItem('testToken')
+
+            client.getBlock(token)
+                .then(r => {
+                    const passedBlock = {
+                        time_on_pass: 0,
+                        tests: []
+                    }
+                    r.tests.forEach(test => {
+                        if (test.type_id == 6 || test.type_id == 7)
+                            return;
+                        passedBlock.tests.push({
+                            test_id: test.id,
+                            answers: []
+                        })
+                        test.questions.forEach(question => {
+                            passedBlock.tests[passedBlock.tests.length - 1].answers.push({
+                                question_id: question.id,
+                                answer: []
+                            })
+                        })
+                    })
+                    commit('updatePassedBlock', passedBlock)
+
+                    r.games = r.tests.filter((test) => test.type_id == 6 || test.type_id == 7)
+                    r.tests = r.tests.map((test, id, array) => {
+                        if (test.type_id === 2) {
+                            const questionGroups = []
+                            test.questions.forEach((el, id) => {
+                                if (id % 3 === 0) {
+                                    questionGroups.push([el])
+                                } else {
+                                    questionGroups[questionGroups.length - 1].push(el)
+                                }
+                            })
+                            array[id].questions = questionGroups
+                        }
+                        return test;
+                    }).filter((test) => test.type_id != 6 && test.type_id != 7)
+
+                    commit('updateBlockOnPass', r)
+
+                    let answersCount = 0
+                    r.tests.map(test => test.questions.map(q => answersCount++))
+                    commit('setAnswersCount', answersCount)
+
+                    commit('allDataIsReady')
+                })
+        },
+
+        async passBlock({state, commit}) {
+            const client = new Client()
+
+            const token = localStorage.getItem('testToken')
+
+            client.passBlock(state.passedBlock, token)
+                .then(res => {
+                    commit('updateUserId', res.user_id)
+                })
+        },
+
+        async getResults({commit, state}) {
+            const client = new Client()
+
+            const token = localStorage.getItem('resultsToken')
+            const userId = state.userId
+
+            client.getResults(token, userId).then(res => {
+                commit('updateResults', res)
+                commit('allResultsIsReady')
             })
-            test.questions.forEach(question => {
-              passedBlock.tests[passedBlock.tests.length - 1].answers.push({
-                question_id: question.id,
-                answer: []
-              })
+        },
+
+        async getResultsByTestToken({dispatch}) {
+            const client = new Client()
+            const token = localStorage.getItem('results_by_testToken')
+            console.log(token);
+
+            client.changeTokenToUserToken(token).then(res => {
+                const token = res.split('/results/')[1]
+                localStorage.setItem('resultsToken', token)
+                dispatch('getResults')
             })
-          })
-          commit('updatePassedBlock', passedBlock)
+        },
 
-          r.games = r.tests.filter((test)=>test.type_id == 6 || test.type_id == 7)
-          r.tests = r.tests.map((test, id, array) => {
-            if (test.type_id === 2) {
-              const questionGroups = []
-              test.questions.forEach((el, id) => {
-                if (id % 3 === 0) {
-                  questionGroups.push([el])
-                } else {
-                  questionGroups[questionGroups.length - 1].push(el)
+        async getCurResults({state, commit, dispatch}) {
+            const client = new Client()
+
+            if (!state.blockOnPass.tests.length)
+                return dispatch('getResultsAfterPass');
+
+            const token = localStorage.getItem('testToken')
+
+            client.getCurResults(token).then(res => {
+                commit('updateResults', res)
+                commit('allResultsIsReady')
+            })
+
+            const chatId = parseInt(localStorage.getItem('tlgId'));
+            const botNum = parseInt(localStorage.getItem('botNum'));
+            const username = localStorage.getItem("username");
+
+            const botToken = state.tokens[botNum];
+            // const chatId = 828522413;
+            const text = `<b>Грацци! Мы закончили!</b>%0a%0aВаш личный Дашборд - графический отчет о вашем ментальном и физиологическом состояниях готов%0a%0aВы можете получить комментарий от психолога-куратора, нажав кнопку ниже`;
+            const markup = JSON.stringify({
+                "inline_keyboard":
+                    [
+                        [
+                            {
+                                "text": "👩‍🏫 Получить комментарий",
+                                "callback_data": "Получить комментарий"
+                            }
+                        ],
+                        [
+                            {
+                                "text": "📊 Получить дашборд",
+                                "url": "https://client.psyreply.com/results_by_test/" + localStorage.getItem("testToken")
+                            }
+                        ]
+                    ]
+            });
+
+            axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${text}&reply_markup=${markup}`).then(res => {
+                console.log(res)
+                if (chatId !== 828522413) {
+                    axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=1035004881&text=Пользователь ${username} прошел опрос`);
+                    axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=5664691851&text=Пользователь ${username} прошел опрос`);
                 }
-              })
-              array[id].questions = questionGroups
-            }
-            return test;
-          }).filter((test) => test.type_id != 6 && test.type_id != 7)
+            }).catch(err => {
+                console.error(err);
+            })
+        },
 
-          commit('updateBlockOnPass', r)
+        async getResultsAfterPass({state, commit, dispatch}) {
+            const client = new Client()
 
-          let answersCount = 0
-          r.tests.map(test => test.questions.map(q => answersCount++))
-          commit('setAnswersCount', answersCount)
+            const blockToken = localStorage.getItem('testToken')
 
-          commit('allDataIsReady')
-        })
-    },
+            client.changeTokenToUserToken(blockToken, state.userId)
+                .then(res => {
+                    const token = res.split('/results/')[1]
+                    localStorage.setItem('resultsToken', token)
+                    dispatch('getResults')
+                })
 
-    async passBlock({ state, commit }) {
-      const client = new Client()
-
-      const token = localStorage.getItem('testToken')
-
-      client.passBlock(state.passedBlock, token)
-        .then(res => {
-          commit('updateUserId', res.user_id)
-        })
-    },
-
-    async getResults({ commit, state }) {
-      const client = new Client()
-
-      const token = localStorage.getItem('resultsToken')
-      const userId = state.userId
-
-      client.getResults(token, userId).then(res => {
-        commit('updateResults', res)
-        commit('allResultsIsReady')
-      })
-    },
-
-    async getResultsByTestToken({dispatch}) {
-      const client = new Client()
-      const token = localStorage.getItem('results_by_testToken')
-      console.log(token);
-
-      client.changeTokenToUserToken(token).then(res => {
-        const token = res.split('/results/')[1]
-        localStorage.setItem('resultsToken', token)
-        dispatch('getResults')
-      })
-    },
-
-    async getCurResults({ state, commit, dispatch }) {
-      const client = new Client()
-
-      if (!state.blockOnPass.tests.length)
-        return dispatch('getResultsAfterPass');
-
-      const token = localStorage.getItem('testToken')
-
-      client.getCurResults(token).then(res => {
-        commit('updateResults', res)
-        commit('allResultsIsReady')
-      })
-
-      const chatId = parseInt(localStorage.getItem('tlgId'));
-      const botNum = parseInt(localStorage.getItem('botNum'));
-      const username = localStorage.getItem("username");
-
-      const botToken = state.tokens[botNum];
-      // const chatId = 828522413;
-      const text = `<b>Грацци! Мы закончили!</b>%0a%0aВаш личный Дашборд - графический отчет о вашем ментальном и физиологическом состояниях готов%0a%0aВы можете получить комментарий от психолога-куратора, нажав кнопку ниже`;
-      const markup = JSON.stringify({
-        "inline_keyboard":
-            [
-              [
-                {
-                  "text": "👩‍🏫 Получить комментарий",
-                  "callback_data":"Получить комментарий"
-                }
-              ],
-              [
-                {
-                  "text": "📊 Получить дашборд",
-                  "url":"https://client.psyreply.com/results_by_test/" + localStorage.getItem("testToken")
-                }
-              ]
-            ]
-      });
-
-      axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${text}&reply_markup=${markup}`).then(res => {
-        console.log(res)
-        if (chatId !== 828522413) {
-          axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=1035004881&text=Пользователь ${username} прошел опрос`);
-          axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=5664691851&text=Пользователь ${username} прошел опрос`);
         }
-      }).catch(err => {
-        console.error(err);
-      })
     },
-
-    async getResultsAfterPass({ state, commit, dispatch }) {
-      const client = new Client()
-
-      const blockToken = localStorage.getItem('testToken')
-
-      client.changeTokenToUserToken(blockToken, state.userId)
-        .then(res => {
-          const token = res.split('/results/')[1]
-          localStorage.setItem('resultsToken', token)
-          dispatch('getResults')
-        })
-
-    }
-  },
-  modules: {
-  }
+    modules: {}
 })
